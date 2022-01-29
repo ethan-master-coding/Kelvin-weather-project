0a7632bdc79bb095cf8ec9a13a0bd1056794cf68
Ethan <92686703+Ethan-Master-Coding@users.noreply.github.com>
2022-01-29 11:10:56 -0500

    Added the code

---
+++ Kelvin.js
@@ -0,0 +1,11 @@
+const kelvin = 0;
+
+const celsius = kelvin - 273;
+
+fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+
+console.log(`The temp is ${fahrenheit} defrees in fahrenheit.`)
+
+newton = Math.floor(celsius * (33/100))
+
+console.log(newton)
